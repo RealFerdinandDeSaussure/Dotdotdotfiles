dfd996c109f188e4c62fd2b19e0dcc081fdabfc2
Pu Anlai <inspectormustache@users.noreply.github.com>
2024-12-01 12:31:30 +0100

    Add cotp helper functions

---
+++ .config/fish/functions/pcotp.fish
@@ -0,0 +1,3 @@
+function pcotp
+    pass show $PASSWORD_STORE_PASS_COTP | cotp --password-stdin $argv
+end
---
+++ .config/fish/functions/renew_ctop.fish
@@ -0,0 +1,13 @@
+function renew_ctop
+    if [ -z "$argv" ]
+        echo "Specify the Aegis encrypted database file to import." >&2
+        return 1
+    else if [ ! -f "$argv" ]
+        echo "$argv is not a valid file." >&2
+        return 1
+    end
+
+    set -q COTP_DB_PATH || set COTP_DB_PATH "$HOME/.local/share/cotp"
+    rm -v {$COTP_DB_PATH}/db.cotp
+    cotp import --path "$argv" --aegis-encrypted
+end
