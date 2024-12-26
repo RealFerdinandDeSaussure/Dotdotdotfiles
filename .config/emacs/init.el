848bb7a322b76968cbbae92928355e559e815c80
Pu Anlai <inspectormustache@users.noreply.github.com>
2024-12-27 00:33:25 +0100

    Move early init to early-init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,26 +1,5 @@
 ;; -*- lexical-binding: t -*-
 
-;; enable sourcing from init scripts in emacs.d/subinits
-(defconst emacs-subinit-dir (expand-file-name "subinits" user-emacs-directory))
-(add-to-list 'load-path emacs-subinit-dir)
-
-;; custom-file handling
-(setq custom-file (expand-file-name "custom.el" emacs-subinit-dir))
-;; provide two custom-file hooks for different init stages
-(defvar °pre-init-custom-hook nil)
-(defvar °post-init-custom-hook nil)
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-;; set up a separate location for backup and temp files
-(defconst emacs-tmp-dir (expand-file-name "auto-save" user-emacs-directory))
-(setq backup-directory-alist
-      `((".*" . ,emacs-tmp-dir)))
-(setq auto-save-file-name-transforms
-      `((".*" ,(concat emacs-tmp-dir "/\\1") t)))
-    (setq auto-save-list-file-prefix
-      emacs-tmp-dir)
-
 (require 'package-management)
 
 (run-hooks '°pre-init-custom-hook) ; everything even earlier can go directly into custom-file
