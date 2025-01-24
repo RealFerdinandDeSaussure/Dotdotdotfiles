84a90f97f0f415d49d907149f64ddd4b0e4c2cee
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-01-24 17:08:11 +0100

    Move contents of completion.el to emacs-extensions.el

--- .config/emacs/subinits/emacs-extensions.el
+++ .config/emacs/subinits/emacs-extensions.el
@@ -163,4 +163,63 @@ Start terminal if it isn't running already."
               (delete-window (get-buffer-window buf)))))
 
 
+(use-package vertico
+  :init
+  (setq completion-ignore-case t
+        read-buffer-completion-ignore-case t
+        read-file-name-completion-ignore-case t)
+  (vertico-mode)
+  :general-config
+  (:keymaps         'vertico-map
+    "M-k"           'previous-history-element
+    "M-j"           'next-history-element)
+  :config
+  (evil-collection-vertico-setup))
+
+(use-package embark
+  :general
+  (:keymaps         '(motion emacs evil-ex-completion-map
+                             evil-ex-search-keymap read-expression-map
+                             minibuffer-local-map)
+   "C-,"            'embark-act
+   "C-;"            'embark-dwim)
+  :config
+  (evil-collection-embark-setup))
+
+(use-package consult
+  :general
+  (general-leader
+    :keymaps        'normal
+    "b"             '°consult-file-buffers
+    "B"             'consult-buffer
+    "I"             'consult-imenu
+    "M"             'consult-flymake
+    "g"             'consult-grep
+    "G"             'consult-git-grep)
+  :config
+  (defvar °°consult--source-file-buffers
+    (list :state #'consult--buffer-state
+          :history 'buffer-name-history
+          :items (lambda () (mapcar #'buffer-name (seq-filter #'buffer-file-name (buffer-list))))))
+
+  (defun °consult-file-buffers ()
+    "Consult menu to switch to file buffers only."
+    (interactive)
+    (consult-buffer '(°°consult--source-file-buffers)))
+
+  (evil-collection-consult-setup))
+
+(use-package marginalia
+  :after vertico
+  :config
+  (marginalia-mode))
+
+(use-package embark-consult
+  :after (consult embark))
+
+(use-package hotfuzz
+  :defer t
+  :init
+  (setq completion-styles '(hotfuzz)))
+
 (provide 'emacs-extensions)
