1dbe60c14bd71a458e9b7dcbacdff138213030fd
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-03-09 12:55:40 +0100

    Return from adbfsmount if no device was detected

--- .config/fish/functions/adbfsmount.fish
+++ .config/fish/functions/adbfsmount.fish
@@ -1,6 +1,11 @@
 function adbfsmount --description "Provide a adbfs mountpoint as $adbfsmnt"
     set adbfs (findmnt -rnt fuse.adbfs | head -n1 | cut -d\  -f1)
 
+    if not adb get-state >/dev/null 2>&1
+        echo "No Android device connected."
+        return 1
+    end
+
     if [ -n "$adbfs" ]
         if set -qg adbfsmnt
             set_color -o; echo "Unmounting \$adbfsmnt ($adbfsmnt)..."; set_color normal
