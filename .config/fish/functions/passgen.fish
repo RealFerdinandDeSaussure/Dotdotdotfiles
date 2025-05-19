caf01d7229e5c848bb340e34bd5de3c466e0217b
Pu Anlai <inspectormustache@users.noreply.github.com>
2025-05-19 13:44:55 +0200

    Add diceware option to passgen

--- .config/fish/functions/passgen.fish
+++ .config/fish/functions/passgen.fish
@@ -1,16 +1,27 @@
-function passgen -a filename
-    if [ -z "$filename" ]
+function passgen
+    argparse -x 'diceware,randstring' 'd/diceware' 'r/randstring' -- $argv
+
+    if [ -z "$argv" ]
         echo "Specify a password file name." >&2
         return 1
     end
 
-    set counter 1
-    set pass_in (randstring)
+    if [ -n "$_flag_randstring" ]
+        set pass_in (randstring)
+    else if [ -n "$_flag_diceware" ]
+        set pass_in (diceware -d" " -w de) || return 1
+    else
+        echo "Please specify either --diceware or --randstring." >&2
+        return 1
+    end
+
+    set counter 2
+    echo "$(set_color -o)Enter line 1$(set_color normal): **************"
     while true
         read -P "$(set_color -o)Enter line $counter$(set_color normal): " input
         test -z "$input" && break
         set -a pass_in $input
         set counter (math $counter + 1)
     end
-    printf "%s\n" $pass_in | pass insert -m $filename
+    printf "%s\n" $pass_in | pass insert -m $argv
 end
