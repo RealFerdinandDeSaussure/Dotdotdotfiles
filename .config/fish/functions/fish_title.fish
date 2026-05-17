6e9aa0b4892970e9abb5a2d4564f06364823022e
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-05-17 20:40:54 +0200

    fish: Simplify window title

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,10 +1,3 @@
 function fish_title
-    # only set title for the terminal emulator
-    if not string match -qe "$TERMINAL" "$TERM"
-        return
-    end
-
-    # standard title
-    echo $_ ' '
-    pwd
+    echo -n "$TERM - $_"
 end
