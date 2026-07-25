b398d4c4f2a7332a9d41628f0e99f9d0a3301e76
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-07-25 21:32:05 +0200

    fish: Calculate base16 hex values on demand with hex2rgb function

--- .config/fish/conf.d/fish_shared_variables.fish
+++ .config/fish/conf.d/fish_shared_variables.fish
@@ -3,16 +3,6 @@ set -g fish_default_variables_set
 set -g fish_escape_delay_ms 10
 set -g fish_function_path_local $HOME/.config/fish/functions
 
-# set base16 derived color variables
-for var in __BASE00 __BASE01 __BASE02 __BASE03 __BASE04 __BASE05 __BASE06 \
-            __BASE07 __BASE08 __BASE09 __BASE0A __BASE0B __BASE0C __BASE0D \
-            __BASE0E __BASE0F
-
-    set -g {$var}_R (printf '%d' 0x(string sub -l 2 $$var))
-    set -g {$var}_G (printf '%d' 0x(string sub -s 3 -l 2 $$var))
-    set -g {$var}_B (printf '%d' 0x(string sub -s 5 -l 2 $$var))
-end
-
 # shell exclusive MEDIA variable
 set -gx MEDIA "/run/media/$USER/"
 
---
+++ .config/fish/functions/hex2rgb.fish
@@ -0,0 +1,16 @@
+function hex2rgb
+    argparse -x 'r,g,b' 'r' 'g' 'b' -- $argv || return 1
+    test (count $argv) -eq 1 || return 1
+    test (string length $argv) -eq 6 || return 1
+
+    switch $argv_opts
+        case -r
+            printf '%d' 0x(string sub -l 2 "$argv")
+        case -g
+            printf '%d' 0x(string sub -s 3 -l 2 "$argv")
+        case -b
+            printf '%d' 0x(string sub -s 5 -l 2 "$argv")
+        case '*'
+            printf '%d' 0x$argv
+    end
+end
