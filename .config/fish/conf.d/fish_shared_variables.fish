699c74b8b21f124c21803ab06eb2321b51eff04c
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-08-14 17:15:49 +0200

    fzf: Add readline-like first and last keybinds

--- .config/fish/conf.d/fish_shared_variables.fish
+++ .config/fish/conf.d/fish_shared_variables.fish
@@ -68,7 +68,7 @@ set -x FONT_TERMINAL "Iosevka Nerd Font"
 set -x FZF_DEFAULT_OPTS "--height 60% --border \
 --color=bg+:-1,bg:-1,spinner:#$__BASE0C,hl:#$__BASE0D,hl+:#$__BASE0D,fg:#$__BASE04,fg+:#$__BASE07 \
 --color=header:#$__BASE0D,info:#$__BASE0A,pointer:#$__BASE0C,marker:#$__BASE0C,prompt:#$__BASE09 \
---bind=alt-j:down,alt-k:up --reverse"
+--bind=alt-j:down,alt-k:up,alt-\<:first,alt-\>:last --reverse"
 
 set -x FZF_DEFAULT_COMMAND "/usr/bin/find -P \$dir -mindepth 1 \( -regex '\.?/snp' -o -path '*/Steam' -o -path '*/.cache' -o -path '*/.git' \) -prune -o -print 2>/dev/null"
 set -x FZF_OVERLAY_OPTS "--no-border --margin 10%,8% --no-height --layout reverse-list"
