1ac70e35a56f6ab1fb6953f3f7079235537f5da2
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-08-20 22:21:07 +0200

    fish: Simplify chksum function

--- .config/fish/functions/chksum.fish
+++ .config/fish/functions/chksum.fish
@@ -1,15 +1,15 @@
 function chksum
-    argparse -n 'chksum' 'b/binary=' -- $argv
+    argparse -n 'chksum' 'h/hash=' -- $argv
     or return
 
-    set -a _flag_binary md5sum
+    set -a _flag_hash md5sum
     
     if [ ! (count $argv) -eq 2  -o ! -f "$argv[-1]" ]
-        echo "syntax: chksum [-b BINARY] CHECKSUM FILE"
+        echo "syntax: chksum [-h HASH_TYPE] CHECKSUM FILE"
         return
     end
 
-    set filehash ($_flag_binary[1] "$argv[-1]" | awk '{print $1}')
+    set filehash ($_flag_hash[1]sum "$argv[-1]" | awk '{print $1}')
     or return
 
     if [ (string lower "$filehash") = (string lower "$argv[-2]") ]
