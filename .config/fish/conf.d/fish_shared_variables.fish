45eb55dfa7891bdfc421100968fb342df63aa217
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-08-28 14:13:00 +0200

    fish: Use default colors in the virtual console

--- .config/fish/conf.d/fish_shared_variables.fish
+++ .config/fish/conf.d/fish_shared_variables.fish
@@ -87,70 +87,72 @@ set fish_default_variables_set
 set fish_escape_delay_ms 10
 set fish_function_path_local $HOME/.config/fish/functions
 
-# default color
-set fish_color_normal --reset
-# commands like echo
-set fish_color_command $__BASE07
-# builtin commands like cd and set - this falls back on the command color if unset
-# set fish_color_builtin
-# user-defined functions - this falls back on the command color if unset
-set fish_color_function $__BASE07 -udotted --underline-color=$__BASE03
-# keywords like if - this falls back on the command color if unset
-set fish_color_keyword $__BASE09
-# quoted text like abc
-set fish_color_quote $__BASE0B
-# IO redirections like >/dev/null
-set fish_color_redirection $__BASE0C --bold
-# process separators like ; and &
-set fish_color_end $__BASE0E
-# syntax errors
-set fish_color_error $__BASE08
-# ordinary command parameters
-set fish_color_param --reset
-# parameters and redirection targets that are filenames (if the file exists)
-set fish_color_valid_path --underline
-# options starting with “-”, up to the first “--” parameter
-set fish_color_option $__BASE0D
-# comments like ‘# important’
-set fish_color_comment --dim
-# selected text in vi visual mode
-set fish_color_selection --reverse
-# parameter expansion operators like * and ~
-set fish_color_operator $__BASE0A
-# character escapes like \n and \x70
-set fish_color_escape $__BASE0F
-# autosuggestions (the proposed rest of a command)
-set fish_color_autosuggestion $__BASE03
-# the ‘^C’ indicator on a canceled command
-set fish_color_cancel $__BASE08
-# history search matches and selected pager items (background only)
-set fish_color_search_match $__BASE07 --italics --bold --background=brblack
-# the current position in the history for commands like dirh and cdh
-set fish_color_history_current -ucurly --underline-color=$__BASE03 --bold
+if tty | grep -q pts # in the tty, we will fall back on the default colorscheme
+    # default color
+    set fish_color_normal --reset
+    # commands like echo
+    set fish_color_command $__BASE07
+    # builtin commands like cd and set - this falls back on the command color if unset
+    # set fish_color_builtin
+    # user-defined functions - this falls back on the command color if unset
+    set fish_color_function $__BASE07 -udotted --underline-color=$__BASE03
+    # keywords like if - this falls back on the command color if unset
+    set fish_color_keyword $__BASE09
+    # quoted text like abc
+    set fish_color_quote $__BASE0B
+    # IO redirections like >/dev/null
+    set fish_color_redirection $__BASE0C --bold
+    # process separators like ; and &
+    set fish_color_end $__BASE0E
+    # syntax errors
+    set fish_color_error $__BASE08
+    # ordinary command parameters
+    set fish_color_param --reset
+    # parameters and redirection targets that are filenames (if the file exists)
+    set fish_color_valid_path --underline
+    # options starting with “-”, up to the first “--” parameter
+    set fish_color_option $__BASE0D
+    # comments like ‘# important’
+    set fish_color_comment --dim
+    # selected text in vi visual mode
+    set fish_color_selection --reverse
+    # parameter expansion operators like * and ~
+    set fish_color_operator $__BASE0A
+    # character escapes like \n and \x70
+    set fish_color_escape $__BASE0F
+    # autosuggestions (the proposed rest of a command)
+    set fish_color_autosuggestion $__BASE03
+    # the ‘^C’ indicator on a canceled command
+    set fish_color_cancel $__BASE08
+    # history search matches and selected pager items (background only)
+    set fish_color_search_match $__BASE07 --italics --bold --background=brblack
+    # the current position in the history for commands like dirh and cdh
+    set fish_color_history_current -ucurly --underline-color=$__BASE03 --bold
 
-# the progress bar at the bottom left corner
-set fish_pager_color_progress $__BASE02 --background=$__BASE05 --bold
-# the background color of a line
-set fish_pager_color_background
-# the prefix string, i.e. the string that is to be completed
-set fish_pager_color_prefix --bold --underline
-# the completion itself, i.e. the proposed rest of the string
-set fish_pager_color_completion --reset
-# the completion description
-set fish_pager_color_description $__BASE0A --italics
-# background of the selected completion
-set fish_pager_color_selected_backgroud $__BASE06
-# prefix of the selected completion
-# set fish_pager_color_selected_prefix
-# suffix of the selected completion
-# set fish_pager_color_selected_competion 
-# description of the selected completion
-set fish_pager_color_selected_description --reset
-# # background of every second unselected completion
-# set fish_pager_color_secondary_background $__BASE07
-# # prefix of every second unselected completion
-# set fish_pager_color_secondary_prefix
-# # suffix of every second unselected completion
-# set fish_pager_color_secondary_completion
-# # description of every second unselected completion
-# set fish_pager_color_secondary_description
+    # the progress bar at the bottom left corner
+    set fish_pager_color_progress $__BASE02 --background=$__BASE05 --bold
+    # the background color of a line
+    set fish_pager_color_background
+    # the prefix string, i.e. the string that is to be completed
+    set fish_pager_color_prefix --bold --underline
+    # the completion itself, i.e. the proposed rest of the string
+    set fish_pager_color_completion --reset
+    # the completion description
+    set fish_pager_color_description $__BASE0A --italics
+    # background of the selected completion
+    set fish_pager_color_selected_backgroud $__BASE06
+    # prefix of the selected completion
+    # set fish_pager_color_selected_prefix
+    # suffix of the selected completion
+    # set fish_pager_color_selected_competion 
+    # description of the selected completion
+    set fish_pager_color_selected_description --reset
+    # # background of every second unselected completion
+    # set fish_pager_color_secondary_background $__BASE07
+    # # prefix of every second unselected completion
+    # set fish_pager_color_secondary_prefix
+    # # suffix of every second unselected completion
+    # set fish_pager_color_secondary_completion
+    # # description of every second unselected completion
+    # set fish_pager_color_secondary_description
+end
