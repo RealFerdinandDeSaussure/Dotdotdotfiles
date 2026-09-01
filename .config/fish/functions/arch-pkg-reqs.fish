eb24d714d884628fa3ab7042f8e444cd7efb810f
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-09-01 18:32:53 +0200

    fish: Add arch-pkg-reqs function

---
+++ .config/fish/functions/arch-pkg-reqs.fish
@@ -0,0 +1,42 @@
+function arch-pkg-reqs
+    argparse 'q/query' 'i/install' -- "$argv"
+    if [ -z "$_flag_q$_flag_v$_flag_i" ]
+        echo "Mode must be set: --query/--install" >&2
+        return 1
+    end
+
+    set pkg_file $HOME/.config/.packages
+    test -f $pkg_file || return 1
+
+    set new_file
+    test -n "$_flag_install" && set i_pkgs
+
+    while read line
+        set -a new_file "$line"
+        string match -qr -- '^\s*$' "$line" && continue
+        string match -qr -- '^\s*#' "$line" && continue
+        set pkg (string split -m1 -f1 : "$line")
+
+        if [ -n "$_flag_install" ]
+            set -a i_pkgs $pkg
+            continue
+        end
+
+        set query (string split -m1 -f2 : "$line")
+
+        if [ -z "$query" ]
+            set query $pkg
+        else
+            set query (string split , $query)
+        end
+
+        for q in query
+            git grep -q "$query" && continue
+            echo "$pkg not verified on system." >&2
+        end
+    end < $pkg_file
+
+    if [ -n "$_flag_install" ]
+        sudo pacman -S --needed $i_pkgs
+    end
+end
