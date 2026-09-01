5b0be8f5f9fb33eb23e2a850614643e31a0df9cd
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-09-01 12:15:03 +0200

    fish: Set default hash to sha256 for chksum function

--- .config/fish/functions/chksum.fish
+++ .config/fish/functions/chksum.fish
@@ -1,16 +1,19 @@
 function chksum
-    argparse -n 'chksum' 'h/hash=' -- $argv
-    or return
+    argparse -n 'chksum' 'h/hash=' -- $argv || return
 
-    set -a _flag_hash md5sum
-    
     if [ ! (count $argv) -eq 2  -o ! -f "$argv[-1]" ]
         echo "syntax: chksum [-h HASH_TYPE] CHECKSUM FILE"
         return
     end
 
-    set filehash ($_flag_hash[1]sum "$argv[-1]" | awk '{print $1}')
-    or return
+    set -a _flag_hash sha256
+    set hash_exc {$_flag_hash[1]}sum
+    if not command -q "$hash_exc"
+        echo "No program $hash_exc found in \$PATH." >/dev/stderr
+        return 1
+    end
+
+    set filehash ($_flag_hash[1]sum "$argv[-1]" | awk '{print $1}') || return
 
     if [ (string lower "$filehash") = (string lower "$argv[-2]") ]
         echo "OK: Checksum matches." > /dev/stderr
