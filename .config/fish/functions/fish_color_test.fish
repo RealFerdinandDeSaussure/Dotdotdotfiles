ccf7877a9df2bc371858599cbd300ba204b6df49
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-09-19 23:53:45 +0200

    fish: Update fish_color_test to use all currently supported colors

--- .config/fish/functions/fish_color_test.fish
+++ .config/fish/functions/fish_color_test.fish
@@ -1,23 +1,35 @@
 function fish_color_test -d 'Print the fish colors'
     set_color $fish_color_normal; echo fish_color_normal; set_color normal
     set_color $fish_color_command; echo fish_color_command; set_color normal
+    set_color $fish_color_builtin; echo fish_color_builtin; set_color normal
+    set_color $fish_color_function; echo fish_color_function; set_color normal
+    set_color $fish_color_keyword; echo fish_color_keyword; set_color normal
     set_color $fish_color_quote; echo fish_color_quote; set_color normal
     set_color $fish_color_redirection; echo fish_color_redirection; set_color normal
     set_color $fish_color_end; echo fish_color_end; set_color normal
     set_color $fish_color_error; echo fish_color_error; set_color normal
     set_color $fish_color_param; echo fish_color_param; set_color normal
+    set_color $fish_color_valid_path; echo fish_color_valid_path; set_color normal
+    set_color $fish_color_option; echo fish_color_option; set_color normal
     set_color $fish_color_comment; echo fish_color_comment; set_color normal
-    set_color $fish_color_match; echo fish_color_match; set_color normal
     set_color $fish_color_selection; echo fish_color_selection; set_color normal
-    set_color $fish_color_search_match; echo fish_color_search_match; set_color normal
     set_color $fish_color_operator; echo fish_color_operator; set_color normal
     set_color $fish_color_escape; echo fish_color_escape; set_color normal
     set_color $fish_color_autosuggestion; echo fish_color_autosuggestion; set_color normal
     set_color $fish_color_cancel; echo fish_color_cancel; set_color normal
-
+    set_color $fish_color_search_match; echo fish_color_search_match; set_color normal
+    set_color $fish_color_history_current; echo fish_color_history_current; set_color normal
+    set_color $fish_pager_color_progress; echo fish_pager_color_progress; set_color normal
+    set_color $fish_pager_color_background; echo fish_pager_color_background; set_color normal
     set_color $fish_pager_color_prefix; echo fish_pager_color_prefix; set_color normal
     set_color $fish_pager_color_completion; echo fish_pager_color_completion; set_color normal
     set_color $fish_pager_color_description; echo fish_pager_color_description; set_color normal
-    set_color $fish_pager_color_progress; echo fish_pager_color_progress; set_color normal
-    set_color $fish_pager_color_secondary; echo fish_pager_color_secondary; set_color normal
+    set_color $fish_pager_color_selected_backgroud; echo fish_pager_color_selected_backgroud; set_color normal
+    set_color $fish_pager_color_selected_prefix; echo fish_pager_color_selected_prefix; set_color normal
+    set_color $fish_pager_color_selected_competion; echo fish_pager_color_selected_competion; set_color normal
+    set_color $fish_pager_color_selected_description; echo fish_pager_color_selected_description; set_color normal
+    set_color $fish_pager_color_secondary_background; echo fish_pager_color_secondary_background; set_color normal
+    set_color $fish_pager_color_secondary_prefix; echo fish_pager_color_secondary_prefix; set_color normal
+    set_color $fish_pager_color_secondary_completion; echo fish_pager_color_secondary_completion; set_color normal
+    set_color $fish_pager_color_secondary_description; echo fish_pager_color_secondary_description; set_color normal
 end
